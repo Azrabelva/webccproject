b528fbc9c9c7102df2961537f96a943ab13f68c5
pashaputri03 <pashaputri31@gmail.com>
2026-01-07 15:05:31 +0700

    Update login_google.php

--- login_google.php
+++ login_google.php
@@ -1,10 +1,10 @@
 <?php
-require 'config.php';
+require 'vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId($GOOGLE_CLIENT_ID);
-$client->setClientSecret($GOOGLE_CLIENT_SECRET);
-$client->setRedirectUri($GOOGLE_REDIRECT_URI);
+$client->setClientId('615293596362-95gc7m4duel9rbujis8mk5jngjalbucf.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('http://localhost:8000/login_google_callback.php');
 $client->setPrompt('select_account');
 $client->addScope("email");
 $client->addScope("profile");
